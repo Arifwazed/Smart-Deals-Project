3868f2abaf247a0a9d294da06ccc4a55d04a266d
DESKTOP-6NHL7HB\arifh <arifhamim11@gmail.com>
2025-11-04 22:55:57 +0600

    loader

--- smart-deal-client/src/components/Home/LoaderSpin.jsx
+++ smart-deal-client/src/components/Home/LoaderSpin.jsx
@@ -3,11 +3,16 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const LoaderSpin = () => {
   return (
+    // <DotLottieReact
+    //   src="https://lottie.host/31a35538-35d6-47dc-afc5-7a493bbccf95/wDo4nCl3tJ.lottie"
+    //   loop
+    //   autoplay
+      
+    // />
     <DotLottieReact
-      src="https://lottie.host/31a35538-35d6-47dc-afc5-7a493bbccf95/wDo4nCl3tJ.lottie"
+      src="https://lottie.host/cf5ad615-215a-45f3-ac77-cad72ed92a4e/oaKf6mtdlR.lottie"
       loop
       autoplay
-      
     />
   );
 };
